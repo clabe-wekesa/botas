14119807e04bfa221cd59e5e12808c76835955bb
Clabe Wekesa <simiyu86wekesa@gmail.com>
2026-07-30 08:55:13 +0200

    Bump version to 0.1.4

--- botas/__init__.py
+++ botas/__init__.py
@@ -3,4 +3,4 @@ from importlib.metadata import PackageNotFoundError, version
 try:
     __version__ = version("botas-rnaseq")
 except PackageNotFoundError:
-    __version__ = "0.1.3"
+    __version__ = "0.1.4"
