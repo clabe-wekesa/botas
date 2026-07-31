e6f749a8db8bad80451aa4b13a52560ad0feaa29
Clabe Wekesa <simiyu86wekesa@gmail.com>
2026-07-31 15:59:21 +0200

    Update CLI behavior and documentation

--- botas/__init__.py
+++ botas/__init__.py
@@ -3,4 +3,4 @@ from importlib.metadata import PackageNotFoundError, version
 try:
     __version__ = version("botas-rnaseq")
 except PackageNotFoundError:
-    __version__ = "0.1.4"
+    __version__ = "0.1.5"
